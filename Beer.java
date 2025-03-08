d91100dee690e74566ac70148250d0cad2f7868a
kantharjbhandary <152303458+kantharjbhandary@users.noreply.github.com>
2025-03-07 20:27:47 -0800

    Add files via upload

---
+++ Beer.java
@@ -0,0 +1,32 @@
+class Beer{
+  public static void beerBrand(String name)
+  {
+	System.out.println(name);
+	
+  }
+  
+  public static void contentOfAlchohol(String name,float quantityOfAlchol)
+  {
+	System.out.println(name);
+	System.out.println(quantityOfAlchol);
+  }
+  
+  public static void beerBrand(String name,String name1)
+  {
+	  System.out.println(name);
+	  System.out.println(name1);
+  }
+  public static void goodOrBad(boolean yesorno)
+  {
+	System.out.println(yesorno);
+  }
+  
+  public static void detailsOfBrand(String beerName,boolean gudorbad,float quantityOfAlchol)
+  {
+	  System.out.println(beerName);
+	  System.out.println(gudorbad);
+	  System.out.println(quantityOfAlchol);
+	  
+  }
+  
+}
---
+++ Broker.java
@@ -0,0 +1,7 @@
+class Broker{
+	public static void brokerage(String vegitable,int quantity)
+	{
+	 System.out.println("This is Boker class "+vegitable+" "+quantity);
+	 Farmer.farm(vegitable,quantity);
+	}
+}
